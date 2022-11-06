c70f24eef1a7cfb37caf33c7737575343609a08e
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2022-11-06 18:32:08 +0400

    edit user profile

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -4,32 +4,116 @@ from aiogram.dispatcher import FSMContext
 
 from bot.handlers.keyboards.inline.user import user_edit
 from bot.handlers.keyboards.inline.user_callback_datas import edit_user_callback
+from bot.states.user.edit_age import EditAge
+from bot.states.user.edit_description import EditDescription
+from bot.states.user.edit_first_name import EditFirstName
+from bot.states.user.edit_last_name import EditLastName
+from bot.states.user.edit_photo import EditPhoto
 from bot.states.user.edit_username import EditUsername
-from database.user.sqlite import get_user_by_id, edit_user
+import database.user.sqlite as user_db
 
 
 async def command_user(message: types.Message):
-    user = await get_user_by_id(user_id=message.from_user.id)
+    user = await user_db.get_user_by_id(user_id=message.from_user.id)
 
     await message.answer(text=f'user id: {user.get("user_id")}\n'
                               f'username: {user.get("username")}\n'
                               f'first name: {user.get("first_name")}\n'
                               f'last name: {user.get("last_name")}\n'
                               f'age: {user.get("age")}\n'
-                              f'description: {user.get("description")}',
-                         reply_markup=user_edit)
+                              f'description: {user.get("description")}', reply_markup=user_edit)
 
     await message.answer_photo(user.get('photo'))
 
 
 async def edit_username_callback(call: types.CallbackQuery):
-    await call.answer(text='enter new username')
+    await call.message.answer(text='enter new username')
     await EditUsername.InsertUsername.set()
 
 
 async def edit_username(message: types.Message, state: FSMContext):
-    await edit_user(user_id=message.from_user.id, username=message.text)
-    await state.finish()
+    try:
+        await user_db.edit_username(user_id=message.from_user.id, username=message.text)
+        await message.answer('successfully')
+    except Exception as er:
+        await message.answer(text=f"Error: {er}")
+    finally:
+        await state.finish()
+
+
+async def edit_first_name_callback(call: types.CallbackQuery):
+    await call.message.answer(text='enter new first name')
+    await EditFirstName.InsertFirstName.set()
+
+
+async def edit_first_name(message: types.Message, state: FSMContext):
+    try:
+        await user_db.edit_first_name(user_id=message.from_user.id, first_name=message.text)
+        await message.answer('successfully')
+    except Exception as er:
+        await message.answer(text=f"Error: {er}")
+    finally:
+        await state.finish()
+
+
+async def edit_last_name_callback(call: types.CallbackQuery):
+    await call.message.answer(text='enter new last name')
+    await EditLastName.InsertLastName.set()
+
+
+async def edit_last_name(message: types.Message, state: FSMContext):
+    try:
+        await user_db.edit_last_name(user_id=message.from_user.id, last_name=message.text)
+        await message.answer('successfully')
+    except Exception as er:
+        await message.answer(text=f"Error: {er}")
+    finally:
+        await state.finish()
+
+
+async def edit_age_callback(call: types.CallbackQuery):
+    await call.message.answer(text='enter new age')
+    await EditAge.InsertAge.set()
+
+
+async def edit_age(message: types.Message, state: FSMContext):
+    try:
+        await user_db.edit_age(user_id=message.from_user.id, age=int(message.text))
+        await message.answer('successfully')
+    except Exception as er:
+        await message.answer(text=f"Error: {er}")
+    finally:
+        await state.finish()
+
+
+async def edit_description_callback(call: types.CallbackQuery):
+    await call.message.answer(text='enter new description')
+    await EditDescription.InsertDescription.set()
+
+
+async def edit_description(message: types.Message, state: FSMContext):
+    try:
+        await user_db.edit_description(user_id=message.from_user.id, description=message.text)
+        await message.answer('successfully')
+    except Exception as er:
+        await message.answer(text=f"Error: {er}")
+    finally:
+        await state.finish()
+
+
+async def edit_photo_callback(call: types.CallbackQuery):
+    await call.message.answer(text='enter new photo')
+    await EditPhoto.InsertPhoto.set()
+
+
+async def edit_photo(message: types.Message, state: FSMContext):
+    try:
+        await user_db.edit_photo(user_id=message.from_user.id, photo=message.photo[-1].file_id)
+        await message.answer('successfully')
+    except Exception as er:
+        await message.answer(text=f"Error: {er}")
+    finally:
+        await state.finish()
 
 
 def register_user(dp: Dispatcher):
@@ -37,3 +121,18 @@ def register_user(dp: Dispatcher):
 
     dp.register_callback_query_handler(edit_username_callback, edit_user_callback.filter(type='edit_username'))
     dp.register_message_handler(edit_username, state=EditUsername.InsertUsername)
+
+    dp.register_callback_query_handler(edit_first_name_callback, edit_user_callback.filter(type='edit_first_name'))
+    dp.register_message_handler(edit_first_name, state=EditFirstName.InsertFirstName)
+
+    dp.register_callback_query_handler(edit_last_name_callback, edit_user_callback.filter(type='edit_last_name'))
+    dp.register_message_handler(edit_last_name, state=EditLastName.InsertLastName)
+
+    dp.register_callback_query_handler(edit_age_callback, edit_user_callback.filter(type='edit_age'))
+    dp.register_message_handler(edit_age, state=EditAge.InsertAge)
+
+    dp.register_callback_query_handler(edit_description_callback, edit_user_callback.filter(type='edit_description'))
+    dp.register_message_handler(edit_description, state=EditDescription.InsertDescription)
+
+    dp.register_callback_query_handler(edit_photo_callback, edit_user_callback.filter(type='edit_photo'))
+    dp.register_message_handler(edit_photo, state=EditPhoto.InsertPhoto,content_types=['photo'])
--- database/user/sqlite.py
+++ database/user/sqlite.py
@@ -4,16 +4,16 @@ import sqlite3 as sq
 async def db_start():
     global db, cur
 
-    db = sq.connect('user.db')
+    db = sq.connect('users.db')
     cur = db.cursor()
 
     cur.execute(
-        'CREATE TABLE IF NOT EXISTS user('
+        'CREATE TABLE IF NOT EXISTS users('
         'user_id TEXT PRIMARY KEY,'
         'username TEXT,'
         'first_name TEXT,'
         'last_name TEXT,'
-        'age TEXT,'
+        'age INT,'
         'description TEXT,'
         'photo TEXT)'
     )
@@ -24,24 +24,54 @@ async def db_start():
 async def create_user(user_id: str, username: str = None, first_name: str = None, last_name: str = None,
                       age: str = None,
                       description: str = None, photo: str = None):
-    user = cur.execute(f'SELECT user_id FROM user WHERE user_id = {user_id}').fetchone()
+    user = cur.execute(f'SELECT user_id FROM users WHERE user_id = {user_id}').fetchone()
 
     if not user:
-        cur.execute('INSERT INTO user VALUES(?,?,?,?,?,?,?)',
+        cur.execute('INSERT INTO users VALUES(?,?,?,?,?,?,?)',
                     (user_id, username, first_name, last_name, age, description, photo))
         db.commit()
 
 
-async def edit_user(user_id, username: str = None, first_name: str = None, last_name: str = None, age: str = None,
-                    description: str = None, photo: str = None):
-    cur.execute(f'UPDATE user'
-                f' SET username = {username}, first_name = {first_name}, last_name = {last_name},'
-                f' age = {age}, description = {description}, photo = {photo}'
-                f' WHERE user_id = {user_id}')
+async def edit_username(user_id: str, username: str):
+    cur.execute('''UPDATE users SET username = "{}" WHERE user_id = "{}"'''.format(username, user_id))
+    db.commit()
+
+
+async def edit_first_name(user_id: str, first_name: str):
+    cur.execute(f'''UPDATE users SET first_name="{first_name}" WHERE user_id = "{user_id}"''')
+    db.commit()
+
+
+async def edit_last_name(user_id: str, last_name: str):
+    cur.execute(f'''UPDATE users SET last_name="{last_name}" WHERE user_id = "{user_id}"''')
+    db.commit()
+
+
+async def edit_age(user_id: str, age: int):
+    cur.execute(f'''UPDATE users SET age={age} WHERE user_id = "{user_id}"''')
+    db.commit()
+
+
+async def edit_description(user_id: str, description: str):
+    cur.execute(f'''UPDATE users SET description="{description}" WHERE user_id = "{user_id}"''')
+    db.commit()
+
+
+async def edit_photo(user_id: str, photo: str):
+    cur.execute(f'''UPDATE users SET photo="{photo}" WHERE user_id = "{user_id}"''')
+    db.commit()
+
+
+async def edit_user(user_id, username: str = None, first_name: str = None,
+                    last_name: str = None, age: int = None, description: str = None, photo: str = None):
+    cur.execute(f'UPDATE users'
+                f' SET username = "{username}", first_name = "{first_name}", last_name = "{last_name}",'
+                f' age = {age}, description = "{description}", photo = "{photo}"'
+                f' WHERE user_id = "{user_id}"')
     db.commit()
 
 
 async def get_user_by_id(user_id: int):
-    user = cur.execute(f'SELECT * FROM user WHERE user_id={user_id}').fetchone()
+    user = cur.execute(f'SELECT * FROM users WHERE user_id={user_id}').fetchone()
     return {'user_id': user[0], 'username': user[1], 'first_name': user[2],
             'last_name': user[3], 'age': user[4], 'description': user[5], 'photo': user[6]}
