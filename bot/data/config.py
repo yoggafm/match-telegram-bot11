a26bb0dca55981672c947a1adbf0909df8213917
yoggafm <118955791+yoggafm@users.noreply.github.com>
2022-11-24 17:01:06 +0700

    Update config.py

--- bot/data/config.py
+++ bot/data/config.py
@@ -1,12 +1,12 @@
 import os
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv('5728932780:AAFQ-Hz0QAQHIV7j7s8hiH0n6Fj9adfKfhQ')
 
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_PATH = f'/webhook/{5728932780:AAFQ-Hz0QAQHIV7j7s8hiH0n6Fj9adfKfhQ}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
